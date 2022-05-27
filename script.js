c126fd6773112fd079338158848845ec41f4f6ca
Kristin Kleyla <clever_disguises@yahoo.com>
2022-05-27 18:49:59 -0400

    wrote test function for saving schedule to local storage

--- script.js
+++ script.js
@@ -7,3 +7,22 @@
 // TODO: Init function that fetches input data from local storage
 // TODO: Convert times to military
 // TODO: Click events for saving inputs
+
+var saveBtnEl = $('.saveBtn');
+var textEl = $('.description');
+
+function saveWork() {
+    var sched = {
+        time: 9,
+        work: textEl.val()
+    }
+
+    var work = JSON.parse(localStorage.getItem('schedule')) || [];
+
+    work.push(sched);
+    console.log(work);
+
+    localStorage.setItem('schedule', JSON.stringify(work));
+}
+
+saveBtnEl.on('click', saveWork);
