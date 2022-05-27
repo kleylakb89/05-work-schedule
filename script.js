930a050f33253fae78a9a8f5d2237a376e744c5b
Kristin Kleyla <clever_disguises@yahoo.com>
2022-05-27 19:21:09 -0400

    testing displaying text in textarea

--- script.js
+++ script.js
@@ -7,22 +7,38 @@
 // TODO: Init function that fetches input data from local storage
 // TODO: Convert times to military
 // TODO: Click events for saving inputs
+// TODO: Update HTML with proper tags and classes
 
 var saveBtnEl = $('.saveBtn');
 var textEl = $('.description');
+var tableEl = $('.table');
 
 function saveWork() {
+    // TODO: Update time key with proper time input
     var sched = {
         time: 9,
         work: textEl.val()
     }
 
     var work = JSON.parse(localStorage.getItem('schedule')) || [];
-
+    
+    // TODO: Overwrite existing storage with matching time
     work.push(sched);
-    console.log(work);
-
+    
     localStorage.setItem('schedule', JSON.stringify(work));
+    displayWork();
+}
+
+function displayWork() {
+    var work = JSON.parse(localStorage.getItem('schedule')) || [];
+    console.log(work[0].time);
+    tableEl.find('.description').text('just checking');
+}
+
+function init() {
+    displayWork();
 }
 
 saveBtnEl.on('click', saveWork);
+
+init();
