c1e260985a1f88c4fcb80bf84a1e6475c98284ab
Kristin Kleyla <clever_disguises@yahoo.com>
2022-05-27 18:35:38 -0400

    made to do list for program

---
+++ script.js
@@ -0,0 +1,9 @@
+// TODO: Display current day at top of calendar
+// TODO: Style timeblocks
+// TODO: Color code timeblocks
+// TODO: Save input textarea into local storage
+// TODO: Keep input display on refresh
+
+// TODO: Init function that fetches input data from local storage
+// TODO: Convert times to military
+// TODO: Click events for saving inputs
