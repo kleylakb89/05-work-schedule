65242bccb355dd6f096466d8e207f25e6b54add6
Kristin Kleyla <clever_disguises@yahoo.com>
2022-05-31 14:51:43 -0400

    updated comments

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 // TODO: Click events for saving inputs
 // TODO: Update HTML with proper tags and classes
 
+// Query relevant selectors - save buttons, textareas, time blocks
 var saveBtnEl = $('.saveBtn');
 var textEl = $('.description');
 var dayEl = $('#currentDay');
@@ -23,10 +24,9 @@ var fifteenEl = $('.fifteen');
 var sixteenEl = $('.sixteen');
 var seventeenEl = $('.seventeen');
 
-schedIndex = 0;
-
+// Function to accept input in textareas and save them to local storage
 function saveWork() {
-    // TODO: Update time key with proper time input
+    // Fetches different textareas input values
     var schedNine = nineEl.val();
     var schedTen = tenEl.val();
     var schedEleven = elevenEl.val();
@@ -37,6 +37,7 @@ function saveWork() {
     var schedSixteen = sixteenEl.val();
     var schedSeventeen = seventeenEl.val();
 
+    // Parses from local storage
     var nine = JSON.parse(localStorage.getItem('hour-9')) || [];
     var ten = JSON.parse(localStorage.getItem('hour-10')) || [];
     var eleven = JSON.parse(localStorage.getItem('hour-11')) || [];
@@ -47,7 +48,7 @@ function saveWork() {
     var sixteen = JSON.parse(localStorage.getItem('hour-16')) || [];
     var seventeen = JSON.parse(localStorage.getItem('hour-17')) || [];
     
-    // TODO: Overwrite existing storage with matching time
+    // Overwrites existing storage with matching time when textarea is updated
 
     nine.pop();
     nine.push(schedNine);
@@ -68,6 +69,7 @@ function saveWork() {
     seventeen.pop();
     seventeen.push(schedSeventeen);
     
+    // saves to local storage
     localStorage.setItem('hour-9', JSON.stringify(nine));
     localStorage.setItem('hour-10', JSON.stringify(ten));
     localStorage.setItem('hour-11', JSON.stringify(eleven));
@@ -78,10 +80,13 @@ function saveWork() {
     localStorage.setItem('hour-16', JSON.stringify(sixteen));
     localStorage.setItem('hour-17', JSON.stringify(seventeen));
 
+    // runs displayWork function so schedule is displayed
     displayWork();
 }
 
+// Function to display what's saved in local storage on page
 function displayWork() {
+    // Parsing values from local storage
     var nine = JSON.parse(localStorage.getItem('hour-9')) || [];
     var ten = JSON.parse(localStorage.getItem('hour-10')) || [];
     var eleven = JSON.parse(localStorage.getItem('hour-11')) || [];
@@ -92,6 +97,7 @@ function displayWork() {
     var sixteen = JSON.parse(localStorage.getItem('hour-16')) || [];
     var seventeen = JSON.parse(localStorage.getItem('hour-17')) || [];
 
+    // displaying text from parsed values in relevant time slots
     nineEl.text(nine);
     tenEl.text(ten);
     elevenEl.text(eleven);
@@ -104,11 +110,14 @@ function displayWork() {
     
 }
 
+// Function to display the current date from moment.js
 function displayDate() {
     dayEl.text(moment().format('dddd, MMMM Do'));
 }
 
+// Function to determine the current time and adjust the classes of those time slots so they are color coded correctly
 function currentTime() {
+    // Looks at the current time when page loads and compares that time with other hour moments
     var now = moment();
     var nineAm = moment('09:00:00', 'hh:mm:ss');
     var tenAm = moment('10:00:00', 'hh:mm:ss');
@@ -120,6 +129,8 @@ function currentTime() {
     var fourPm = moment('16:00:00', 'hh:mm:ss');
     var fivePm = moment('17:00:00', 'hh:mm:ss');
     var sixPm = moment('18:00:00', 'hh:mm:ss');
+
+    // Adds a class for .css styling based on the current time
     if (moment(now).isBefore(nineAm)) {
         textEl.addClass('future');
     } else if (moment(now).isBetween(nineAm, tenAm)) {
@@ -217,13 +228,15 @@ function currentTime() {
     }
 }
 
+// Initializing function to display the items saved in local storage, today's date, and the color coding for the sections
 function init() {
     displayWork();
     displayDate();
     currentTime();
 }
 
-
+// event listener for the save buttons
 saveBtnEl.on('click', saveWork);
 
+// starts the page with the init function
 init();
